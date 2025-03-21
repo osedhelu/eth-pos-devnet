f6f428ac7c92b5039b41d871368b85dae7d09920
osedhelu <osedhelu@gmail.com>
2025-03-20 23:33:11 -0500

    Modifica `genesis.json` para cambiar el valor de `terminalTotalDifficulty` a formato hexadecimal. Actualiza el script `init-nodes.sh` para limpiar directorios existentes antes de la inicialización de nodos y ajusta el comando de inicialización para incluir la opción `--state.scheme=path`.

--- init-nodes.sh
+++ init-nodes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Limpiar directorios anteriores si existen
+rm -rf bootnode node1 node2
+
 # Crear directorios
 mkdir -p bootnode node1 node2
 
@@ -8,19 +11,22 @@ echo "${NODE_PASS}" > node1/password.txt
 echo "${NODE_PASS}" > node2/password.txt
 
 # Inicializar cada nodo con genesis.json
+echo "Inicializando bootnode..."
 docker run --rm -v $(pwd)/genesis.json:/genesis.json \
   -v $(pwd)/bootnode:/root/.ethereum \
-  ethereum/client-go:stable init /genesis.json
+  ethereum/client-go:stable init --state.scheme=path /genesis.json
 
+echo "Inicializando node1..."
 docker run --rm -v $(pwd)/genesis.json:/genesis.json \
   -v $(pwd)/node1:/root/.ethereum \
-  ethereum/client-go:stable init /genesis.json
+  ethereum/client-go:stable init --state.scheme=path /genesis.json
 
+echo "Inicializando node2..."
 docker run --rm -v $(pwd)/genesis.json:/genesis.json \
   -v $(pwd)/node2:/root/.ethereum \
-  ethereum/client-go:stable init /genesis.json
+  ethereum/client-go:stable init --state.scheme=path /genesis.json
 
 # Dar permisos
-chmod -R 777 bootnode node1 node2
+sudo chmod -R 777 bootnode node1 node2
 
 echo "Nodos inicializados correctamente. Ahora puedes ejecutar 'docker compose up'" 
